4949e98d9f2df6c812a5d026cde387de3ff7e569
Scray005 <and.005@hotmail.com>
2024-03-01 14:22:29 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+    let count = 0;
+    const counterElement = document.getElementById('counter');
+
+    function incrementCounter() {
+        count++;
+        counterElement.textContent = count;
+    }
+
+    function resetCounter() {
+        count = 0;
+        counterElement.textContent = count;
+    }
+
+    counterElement.addEventListener('click', incrementCounter);
