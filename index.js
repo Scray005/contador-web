55e8e85ac0c080bd9c6ec219b6563406e3a71857
Andres <and.005@hotmail.com>
2024-03-01 15:04:57 -0500

    se realizo el cambio

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-        let count = 0;
-        const counterElement = document.getElementById('counter');
+let count = 0;
+const counterElement = document.getElementById('counter');
 
-        function incrementCounter() {
-            count++;
-            counterElement.textContent = count;
-        }
+function incrementCounter() {
+    count++;
+    counterElement.textContent = count;
+}
 
-        function resetCounter() {
-            count = 0;
-            counterElement.textContent = count;
-        }
+function resetCounter() {
+    count = 0;
+    counterElement.textContent = count;
+}
 
-        counterElement.addEventListener('touchstart', handleTouchStart);
-        counterElement.addEventListener('touchend', handleTouchEnd);
-        counterElement.addEventListener('click', incrementCounter);
+function handleTouchStart() {
+    incrementCounter();
+}
 
-        let touchStartX = 0;
+function confirmReset() {
+    const userResponse = confirm("¿Quieres reiniciar el contador?");
 
-        function handleTouchStart(event) {
-            touchStartX = event.touches[0].clientX;
-        }
-
-        function handleTouchEnd(event) {
-            const touchEndX = event.changedTouches[0].clientX;
-            const touchThreshold = 50; // Ajusta este valor según sea necesario
-
-            if (Math.abs(touchEndX - touchStartX) < touchThreshold) {
-                incrementCounter();
-            }
-        }
+    if (userResponse) {
+        resetCounter();
+    }
+}
